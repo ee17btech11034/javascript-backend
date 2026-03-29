7e5ec05715dd5ae901020ae0f8fccf9fa7b56da6
ee17btech11034 <asiwalraja@gmail.com>
2026-03-29 11:40:29 +0530

    backend setup, files, folders, prettier tool

--- backend/Models/hospital/hospital.models.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose'
-
-const hospitalSchema = mongoose.Schema({}, {timestamps: true})
-
-
-export const hospitalModel = mongoose.models.Hospital || mongoose.model("Hospital", hospitalSchema)
--- backend/Models/hospital/modeical_records.models.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose'
-
-const medicalRecordSchema = mongoose.Schema({}, {timestamps: true})
-
-
-export const medicalRecordModel = mongoose.models.MedicalRecord || mongoose.model("MedicalRecord", medicalRecordSchema)
